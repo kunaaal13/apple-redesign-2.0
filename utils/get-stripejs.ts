87bab5b99794c1c786425c1787045ac00e07a510
Kunaaal13 <mrkunalyadav7@gmail.com>
2022-08-28 14:13:33 +0530

    build finished

---
+++ utils/get-stripejs.ts
@@ -0,0 +1,17 @@
+/**
+ * This is a singleton to ensure we only instantiate Stripe once.
+ */
+
+import { loadStripe, Stripe } from "@stripe/stripe-js";
+
+// https://vercel.com/guides/getting-started-with-nextjs-typescript-stripe#loading-stripe.js
+// Singleton Pattern
+let stripePromise: Promise<Stripe | null>;
+const getStripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+  }
+  return stripePromise;
+};
+
+export default getStripe;
